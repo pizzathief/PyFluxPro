d2c3cef8ed62ce2a687dd54b449db2257ba39fff
Peter Isaac <pisaac.ozflux@gmail.com>
2017-08-13 08:59:00 +1000

    Change of version number from V0.1.0 to V0.1.1.

--- scripts/cfg.py
+++ scripts/cfg.py
@@ -1,3 +1,10 @@
 version_name = "PyFluxPro"
-version_number = "V0.1.0"
+version_number = "V0.1.1"
 # V0.1.0 - copy of OzFluxQC V2.9.6f
+# V0.1.1 - cumulative changes up to 13/07/2017
+#        - major change to qcutils.CreateSeries()
+#          - deprecated "FList=" argument
+#          - made all arguments compulsory
+#        - QC flag generated for each series immediately
+#          prior to qcutils.CreateSeries() based solely
+#          on series data mask
